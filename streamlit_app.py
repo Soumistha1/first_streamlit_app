5256aa2fe2dd9a5df7ab235ab2d921b37e3095ed
Soumistha1 <148743522+Soumistha1@users.noreply.github.com>
2023-10-23 11:51:52 +0530

    Update streamlit_app.py
    Added the text or options available for breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry oatmeal & Omega 3')
+Streamlit.text('Hard boiled Free Range Eggs')
+streamlit.text('Kale, spinach & Rocket Smoothie')
